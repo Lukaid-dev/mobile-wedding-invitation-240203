489ccde60f90eb986512f3b52beafd8be44e2ce1
Seongwoo_Lukaid <crescent70004536@gmail.com>
2023-12-21 01:10:23 +0900

    카카오 브라우저 방지

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { Outlet } from 'react-router-dom';
 import Header from './header/Header';
 
+import KakaoInAppBrowserDetect from './KakaoInAppBrowserDetect';
+
 function App() {
   // router에 따라 css를 다르게 적용해야 함
 
   return (
-    <div className="flex items-center justify-center">
-      <div className="w-full bg-gradient-to-b from-bg_from to-bg_to mix-blend-overlay sm:w-2/3 md:w-1/2 lg:w-2/5 xl:w-1/3">
-        <Header />
-        <Outlet />
+    <KakaoInAppBrowserDetect>
+      <div className="flex items-center justify-center">
+        <div className="w-full bg-gradient-to-b from-bg_from to-bg_to mix-blend-overlay sm:w-2/3 md:w-1/2 lg:w-2/5 xl:w-1/3">
+          <Header />
+          <Outlet />
+        </div>
       </div>
-    </div>
+    </KakaoInAppBrowserDetect>
   );
 }
 
---
+++ src/KakaoInAppBrowserDetect.tsx
@@ -0,0 +1,30 @@
+import { PropsWithChildren } from 'react';
+import useKakaoInAppBrowserDetect from './hooks/useKakaoInAppBrowserDetect';
+import { useEffect } from 'react';
+
+const KakaoInAppBrowserDetect: React.FC<PropsWithChildren> = ({ children }) => {
+  const { isKakaoInAppBrowser, moveOtherBrowser } =
+    useKakaoInAppBrowserDetect();
+
+  useEffect(() => {
+    if (isKakaoInAppBrowser) moveOtherBrowser();
+  }, [isKakaoInAppBrowser, moveOtherBrowser]);
+
+  if (isKakaoInAppBrowser)
+    return (
+      <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center">
+          카카오 인앱 브라우저에서는 정상적으로 작동하지 않습니다. <br /> 외부
+          브라우저로 이동합니다.
+        </div>
+        <button
+          className="mx-4 mb-8 flex w-full items-center justify-center rounded-lg bg-gray-400 py-4"
+          onClick={moveOtherBrowser}>
+          브라우저에서 열기
+        </button>
+      </div>
+    );
+  else return <>{children}</>;
+};
+
+export default KakaoInAppBrowserDetect;
