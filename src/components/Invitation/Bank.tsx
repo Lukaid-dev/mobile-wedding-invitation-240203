7610c62f91ece370a8f100032dc11548032bedfb
Seongwoo_Lukaid <crescent70004536@gmail.com>
2023-12-22 00:34:36 +0900

    계좌번호 복사

--- src/components/Invitation/Bank.tsx
+++ src/components/Invitation/Bank.tsx
@@ -3,7 +3,10 @@ import Divider from '../Divider';
 import KPButton from './KPButton';
 import Box from './Box';
 
-import kb from '../../assets/kb.png';
+import toss from '../../assets/toss.png';
+import woori from '../../assets/woori.png';
+import hana from '../../assets/hana.png';
+import nh from '../../assets/nh.png';
 
 export default function Bank() {
   return (
@@ -15,26 +18,26 @@ export default function Bank() {
         <div className="flex items-stretch justify-between">
           <div className="flex">이성우</div>
           <div className="flex items-center justify-center gap-1">
-            <img src={kb} alt="" className="h-4 w-4" />
-            <p>국민 123456-78-901234</p>
+            <img src={toss} alt="토스뱅크" className="h-4 w-4" />
+            <p>토스 1000-6586-0069</p>
           </div>
         </div>
         <div className="flex gap-2">
-          <CopyButton />
+          <CopyButton bank="toss" />
           <KPButton />
         </div>
         <div className="my-4">
           <Divider />
         </div>
         <div className="flex items-stretch justify-between">
-          <div className="flex">이성우</div>
+          <div className="flex">문일순</div>
           <div className="flex items-center justify-center gap-1">
-            <img src={kb} alt="" className="h-4 w-4" />
-            <p>국민 123456-78-901234</p>
+            <img src={woori} alt="우리은행" className="h-4 w-4" />
+            <p>우리 1002-943-618487</p>
           </div>
         </div>
         <div className="flex gap-2">
-          <CopyButton />
+          <CopyButton bank="woori" />
           <KPButton />
         </div>
       </div>
@@ -46,26 +49,26 @@ export default function Bank() {
         <div className="flex items-stretch justify-between">
           <div className="flex">최예주</div>
           <div className="flex items-center justify-center gap-1">
-            <img src={kb} alt="" className="h-4 w-4" />
-            <p>국민 123456-78-901234</p>
+            <img src={hana} alt="하나은행" className="h-4 w-4" />
+            <p>하나 287-910593-11307</p>
           </div>
         </div>
         <div className="flex gap-2">
-          <CopyButton />
+          <CopyButton bank="hana" />
           <KPButton />
         </div>
         <div className="my-4">
           <Divider />
         </div>
         <div className="flex items-stretch justify-between">
-          <div className="flex">최예주</div>
+          <div className="flex">최상철</div>
           <div className="flex items-center justify-center gap-1">
-            <img src={kb} alt="" className="h-4 w-4" />
-            <p>국민 123456-78-901234</p>
+            <img src={nh} alt="농협은행" className="h-4 w-4" />
+            <p>농협 352-1737-6288-23</p>
           </div>
         </div>
         <div className="flex gap-2">
-          <CopyButton />
+          <CopyButton bank="nh" />
           <KPButton />
         </div>
       </div>
--- src/components/Invitation/CopyButton.tsx
+++ src/components/Invitation/CopyButton.tsx
@@ -1,10 +1,37 @@
+import toast, { Toaster } from 'react-hot-toast';
 import copy from '../../assets/copy.svg';
 
-export default function CopyButton() {
+type Bank = 'toss' | 'woori' | 'hana' | 'nh';
+
+export default function CopyButton({ bank }: { bank: Bank }) {
+  const bankAccounts: { [K in Bank]: string } = {
+    toss: '1000-6586-0069',
+    woori: '1002-943-618487',
+    hana: '287-910593-11307',
+    nh: '352-1737-6288-23',
+  };
+
+  const bankNames: { [K in Bank]: string } = {
+    toss: '토스뱅크',
+    woori: '우리은행',
+    hana: '하나은행',
+    nh: '농협은행',
+  };
+
+  const copyToClipboard = (text: string) => {
+    navigator.clipboard.writeText(text);
+    toast.success('계좌번호가 복사되었습니다.');
+  };
+
   return (
-    <div className="flex h-10 w-full items-center justify-center gap-1 rounded-lg border border-gray-400">
+    <div
+      className="flex h-10 w-full items-center justify-center gap-1 rounded-lg border border-gray-400"
+      onClick={() => {
+        copyToClipboard(`${bankNames[bank]} ${bankAccounts[bank]}`);
+      }}>
       <img src={copy} alt="copy" className="h-5" />
       <p className="font-bold">복사</p>
+      <Toaster position="bottom-center" />
     </div>
   );
 }
