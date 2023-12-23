a33b153108857a19612a27ade85aa40a88433e25
Seongwoo_Lukaid <crescent70004536@gmail.com>
2023-12-24 07:51:42 +0900

    카카오톡 공유하기 이미지 변경

--- src/components/Invitation/Share.tsx
+++ src/components/Invitation/Share.tsx
@@ -25,7 +25,7 @@ const Share = () => {
         title: '성우 ❤️ 예주 결혼합니다.',
         description: '2024. 02. 03. 15:20',
         imageUrl:
-          'https://res.cloudinary.com/dxahuoqco/image/upload/v1703169023/cdnhvump79tmoh6x5gga.png',
+          'https://firebasestorage.googleapis.com/v0/b/wedding-invitation-240203.appspot.com/o/preview.png?alt=media&token=55ffdd22-b2cc-4b8e-9b4e-33abd58e9c59',
         link: {
           mobileWebUrl: url,
           webUrl: url,
