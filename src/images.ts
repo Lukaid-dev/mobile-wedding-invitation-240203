3127390e5457b63742e6d70f64eb8554cd7099c1
Seongwoo_Lukaid <crescent70004536@gmail.com>
2024-01-03 12:57:27 +0900

    이미지 스토리지

--- src/images.ts
+++ src/images.ts
@@ -23,106 +23,106 @@ import thumbnail_19 from './assets/250/thumbnail_19.jpg';
 import thumbnail_20 from './assets/250/thumbnail_20.jpg';
 
 // Main
-import main_01 from './assets/1000/main_01.jpg';
-import main_02 from './assets/1000/main_02.jpg';
-import main_03 from './assets/1000/main_03.jpg';
-import main_04 from './assets/1000/main_04.jpg';
-import main_05 from './assets/1000/main_05.jpg';
-import main_06 from './assets/1000/main_06.jpg';
-import main_07 from './assets/1000/main_07.jpg';
-import main_08 from './assets/1000/main_08.jpg';
-import main_09 from './assets/1000/main_09.jpg';
-import main_10 from './assets/1000/main_10.jpg';
-import main_11 from './assets/1000/main_11.jpg';
-import main_12 from './assets/1000/main_12.jpg';
-import main_13 from './assets/1000/main_13.jpg';
-import main_14 from './assets/1000/main_14.jpg';
-import main_15 from './assets/1000/main_15.jpg';
-import main_16 from './assets/1000/main_16.jpg';
-import main_17 from './assets/1000/main_17.jpg';
-import main_18 from './assets/1000/main_18.jpg';
-import main_19 from './assets/1000/main_19.jpg';
-import main_20 from './assets/1000/main_20.jpg';
+// import main_01 from './assets/1000/main_01.jpg';
+// import main_02 from './assets/1000/main_02.jpg';
+// import main_03 from './assets/1000/main_03.jpg';
+// import main_04 from './assets/1000/main_04.jpg';
+// import main_05 from './assets/1000/main_05.jpg';
+// import main_06 from './assets/1000/main_06.jpg';
+// import main_07 from './assets/1000/main_07.jpg';
+// import main_08 from './assets/1000/main_08.jpg';
+// import main_09 from './assets/1000/main_09.jpg';
+// import main_10 from './assets/1000/main_10.jpg';
+// import main_11 from './assets/1000/main_11.jpg';
+// import main_12 from './assets/1000/main_12.jpg';
+// import main_13 from './assets/1000/main_13.jpg';
+// import main_14 from './assets/1000/main_14.jpg';
+// import main_15 from './assets/1000/main_15.jpg';
+// import main_16 from './assets/1000/main_16.jpg';
+// import main_17 from './assets/1000/main_17.jpg';
+// import main_18 from './assets/1000/main_18.jpg';
+// import main_19 from './assets/1000/main_19.jpg';
+// import main_20 from './assets/1000/main_20.jpg';
 
 export const images: imagesType = {
   '01': {
     thumbnail: thumbnail_01,
-    main: main_01,
+    main: 'https://firebasestorage.googleapis.com/v0/b/wedding-invitation-240203.appspot.com/o/1000%2Fmain_01.jpg?alt=media&token=f3dd15d9-4933-47a8-beae-1275ccaa5c69',
   },
   '02': {
     thumbnail: thumbnail_02,
-    main: main_02,
+    main: 'https://firebasestorage.googleapis.com/v0/b/wedding-invitation-240203.appspot.com/o/1000%2Fmain_02.jpg?alt=media&token=1b3cbd9b-3e5b-4311-a7b9-7a6d01577149',
   },
   '03': {
     thumbnail: thumbnail_03,
-    main: main_03,
+    main: 'https://firebasestorage.googleapis.com/v0/b/wedding-invitation-240203.appspot.com/o/1000%2Fmain_03.jpg?alt=media&token=6483fc9b-d80c-4586-80f0-338875796f19',
   },
   '04': {
     thumbnail: thumbnail_04,
-    main: main_04,
+    main: 'https://firebasestorage.googleapis.com/v0/b/wedding-invitation-240203.appspot.com/o/1000%2Fmain_04.jpg?alt=media&token=66915ccb-a6c4-44ed-aecb-88aa026e69eb',
   },
   '05': {
     thumbnail: thumbnail_05,
-    main: main_05,
+    main: 'https://firebasestorage.googleapis.com/v0/b/wedding-invitation-240203.appspot.com/o/1000%2Fmain_05.jpg?alt=media&token=368c5748-1853-449d-8955-5dee7218d89d',
   },
   '06': {
     thumbnail: thumbnail_06,
-    main: main_06,
+    main: 'https://firebasestorage.googleapis.com/v0/b/wedding-invitation-240203.appspot.com/o/1000%2Fmain_06.jpg?alt=media&token=140ecd5a-fcf7-42a3-9fb1-684f81c09099',
   },
   '07': {
     thumbnail: thumbnail_07,
-    main: main_07,
+    main: 'https://firebasestorage.googleapis.com/v0/b/wedding-invitation-240203.appspot.com/o/1000%2Fmain_07.jpg?alt=media&token=0bf97857-8066-4126-b1c6-33cfa9a91ee3',
   },
   '08': {
     thumbnail: thumbnail_08,
-    main: main_08,
+    main: 'https://firebasestorage.googleapis.com/v0/b/wedding-invitation-240203.appspot.com/o/1000%2Fmain_08.jpg?alt=media&token=9e4e5ecd-f027-4a79-81ff-7ac1268fac0c',
   },
   '09': {
     thumbnail: thumbnail_09,
-    main: main_09,
+    main: 'https://firebasestorage.googleapis.com/v0/b/wedding-invitation-240203.appspot.com/o/1000%2Fmain_09.jpg?alt=media&token=72817f16-b56a-4a80-9d35-60e120082970',
   },
   '10': {
     thumbnail: thumbnail_10,
-    main: main_10,
+    main: 'https://firebasestorage.googleapis.com/v0/b/wedding-invitation-240203.appspot.com/o/1000%2Fmain_10.jpg?alt=media&token=2985f498-ceda-4d39-bc3d-2bdac8b662bb',
   },
   '11': {
     thumbnail: thumbnail_11,
-    main: main_11,
+    main: 'https://firebasestorage.googleapis.com/v0/b/wedding-invitation-240203.appspot.com/o/1000%2Fmain_11.jpg?alt=media&token=5ca1f4c3-cb60-4d88-87ef-5434119a2a50',
   },
   '12': {
     thumbnail: thumbnail_12,
-    main: main_12,
+    main: 'https://firebasestorage.googleapis.com/v0/b/wedding-invitation-240203.appspot.com/o/1000%2Fmain_12.jpg?alt=media&token=0cc0b6cd-c073-44d2-8f3c-aa7c68caa886',
   },
   '13': {
     thumbnail: thumbnail_13,
-    main: main_13,
+    main: 'https://firebasestorage.googleapis.com/v0/b/wedding-invitation-240203.appspot.com/o/1000%2Fmain_13.jpg?alt=media&token=8e2212df-368b-44b4-8090-740fc00004fc',
   },
   '14': {
     thumbnail: thumbnail_14,
-    main: main_14,
+    main: 'https://firebasestorage.googleapis.com/v0/b/wedding-invitation-240203.appspot.com/o/1000%2Fmain_14.jpg?alt=media&token=d1ebc573-8dac-47b5-94d6-5b39eeee94d7',
   },
   '15': {
     thumbnail: thumbnail_15,
-    main: main_15,
+    main: 'https://firebasestorage.googleapis.com/v0/b/wedding-invitation-240203.appspot.com/o/1000%2Fmain_15.jpg?alt=media&token=cb42c127-418f-48f3-b7e9-c32d30bdcb28',
   },
   '16': {
     thumbnail: thumbnail_16,
-    main: main_16,
+    main: 'https://firebasestorage.googleapis.com/v0/b/wedding-invitation-240203.appspot.com/o/1000%2Fmain_16.jpg?alt=media&token=580c9dee-1ea5-4855-953a-d06640c56e3e',
   },
   '17': {
     thumbnail: thumbnail_17,
-    main: main_17,
+    main: 'https://firebasestorage.googleapis.com/v0/b/wedding-invitation-240203.appspot.com/o/1000%2Fmain_17.jpg?alt=media&token=36b4a81b-8cdf-4192-a13e-3dbccdd8bfbc',
   },
   '18': {
     thumbnail: thumbnail_18,
-    main: main_18,
+    main: 'https://firebasestorage.googleapis.com/v0/b/wedding-invitation-240203.appspot.com/o/1000%2Fmain_18.jpg?alt=media&token=44bfe1c0-0faf-4763-aae9-67cd77a5f7d1',
   },
   '19': {
     thumbnail: thumbnail_19,
-    main: main_19,
+    main: 'https://firebasestorage.googleapis.com/v0/b/wedding-invitation-240203.appspot.com/o/1000%2Fmain_19.jpg?alt=media&token=8d2407b9-db7f-48f0-bd71-49e20f5e7478',
   },
   '20': {
     thumbnail: thumbnail_20,
-    main: main_20,
+    main: 'https://firebasestorage.googleapis.com/v0/b/wedding-invitation-240203.appspot.com/o/1000%2Fmain_20.jpg?alt=media&token=e63d20d7-adf1-468b-9eca-8cc4c482722f',
   },
 };
